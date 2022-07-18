8b82da192f73cffe02958cd30d7d61ee2e163dd4
Dave Chmura <dschmura@humbledaisy.com>
2022-07-18 11:06:13 -0400

    FIX: Removed duplicate bullet initializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,13 +2,12 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
+    Bullet.enable = true
+    Bullet.alert = false
     Bullet.bullet_logger = true
-    Bullet.console       = true
-  # Bullet.growl         = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = false
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -77,12 +76,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = false
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = false
-  end
+
 end
