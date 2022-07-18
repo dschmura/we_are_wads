c68859d3379674b42ef0d5e44a7802d2c53c86e4
David Chmura <dschmura@humbledaisy.com>
2022-07-18 14:05:53 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  scope controller: :static_pages do
+    get :about
+    get :contact
+    get :index
+    get :privacy
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "static_pages#index"
+end
