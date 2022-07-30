037049fef334056090a434dfa5c01c8afbcdb52e
Dave Chmura <dschmura@humbledaisy.com>
2022-07-30 11:52:45 -0400

    ADD: first, last, preferred, and nick name fields to the User model

---
+++ db/migrate/20220730154619_add_names_to_user.rb
@@ -0,0 +1,8 @@
+class AddNamesToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :preferred_name, :string
+    add_column :users, :nick_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_150045) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_154619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_150045) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "preferred_name"
+    t.string "nick_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
